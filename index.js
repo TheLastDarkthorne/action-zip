28e7b76eee8d4e6f0bcb70114ed2343e5b0dd415
TheLastDarkthorne <36555422+TheLastDarkthorne@users.noreply.github.com>
2022-10-27 13:48:17 -0300

    multifiles again

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const path = require("path");
 const AdmZip = require("adm-zip");
 const core = require("@actions/core");
 
-const files = core.getInput("files");
 const dest = core.getInput("dest");
 const recursive = core.getInput("recursive") === "true";
-const multifiles = core.getMultilineInput("multifiles");
+const files = core.getMultilineInput("files");
 console.log(`Ready to zip "${files}" into ${dest}`);
 
 const zip = new AdmZip();
 
-if (!multifiles) {
-files.split(" ").forEach(fileName => {
+files.forEach(fileName => {
   const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
 
   if (!fs.existsSync(filePath)) {
@@ -33,29 +31,6 @@ files.split(" ").forEach(fileName => {
 
   console.log(`  - ${fileName}`);
 });
-} else {
-  multifiles.forEach(fileName => {
-    const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
-  
-    if (!fs.existsSync(filePath)) {
-      console.log(`  - ${fileName} (Not Found)`);
-      return;
-    }
-  
-    const dir = path.dirname(fileName);
-    const stats = fs.lstatSync(filePath);
-  
-    if (stats.isDirectory()) {
-      const zipDir = dir === "." ? fileName : dir;
-      zip.addLocalFolder(filePath, !recursive && zipDir);
-    } else {
-      const zipDir = dir === "." ? "" : dir;
-      zip.addLocalFile(filePath, !recursive && zipDir);
-    }
-  
-    console.log(`  - ${fileName}`);
-  });
-}
 
 const destPath = path.join(process.env.GITHUB_WORKSPACE, dest);
 
