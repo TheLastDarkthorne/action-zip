7111c2f06838bcf40bc2012a3688c639e1723381
TheLastDarkthorne <36555422+TheLastDarkthorne@users.noreply.github.com>
2022-10-27 13:25:43 -0300

    Multifiles

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const core = require("@actions/core");
 const files = core.getInput("files");
 const dest = core.getInput("dest");
 const recursive = core.getInput("recursive") === "true";
-
+const multifiles = core.getMultilineInput("multifiles");
 console.log(`Ready to zip "${files}" into ${dest}`);
 
 const zip = new AdmZip();
 
+if (!multifiles) {
 files.split(" ").forEach(fileName => {
   const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
 
@@ -32,6 +33,29 @@ files.split(" ").forEach(fileName => {
 
   console.log(`  - ${fileName}`);
 });
+} else {
+  multifiles.forEach(fileName => {
+    const filePath = path.join(process.env.GITHUB_WORKSPACE, fileName);
+  
+    if (!fs.existsSync(filePath)) {
+      console.log(`  - ${fileName} (Not Found)`);
+      return;
+    }
+  
+    const dir = path.dirname(fileName);
+    const stats = fs.lstatSync(filePath);
+  
+    if (stats.isDirectory()) {
+      const zipDir = dir === "." ? fileName : dir;
+      zip.addLocalFolder(filePath, !recursive && zipDir);
+    } else {
+      const zipDir = dir === "." ? "" : dir;
+      zip.addLocalFile(filePath, !recursive && zipDir);
+    }
+  
+    console.log(`  - ${fileName}`);
+  });
+}
 
 const destPath = path.join(process.env.GITHUB_WORKSPACE, dest);
 
